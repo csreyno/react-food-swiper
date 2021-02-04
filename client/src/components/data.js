0a5c613ff3cb932eea89505e054bcd628ed124cd
Chad Reynolds <chad.s.reynolds@gmail.com>
2021-02-04 10:51:47 -0500

    added swiper with modified css

---
+++ client/src/components/data.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    pics: [
+      "https://images.unsplash.com/photo-1514924801778-1db0aba75e9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+    ],
+    name: "Chloe",
+    age: 29,
+    distance: "3 miles away",
+    text:
+      "I keep calling the plumber round, and he keeps fixing stuff. Where am I going wrong?"
+  },
+  {
+    pics: [
+      "https://images.unsplash.com/photo-1456885284447-7dd4bb8720bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
+      "https://images.unsplash.com/photo-1532635270-c09dac425ca9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+    ],
+    name: "Jane",
+    age: 22,
+    distance: "2 miles away",
+    text:
+      "On the first date I will carve our initials in a tree. It's the most romantic way to let you know I have a knife."
+  },
+  {
+    pics: [
+      "https://images.unsplash.com/photo-1584287981937-67ab60932edf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    ],
+    name: "Savannah",
+    age: 18,
+    distance: "1 mile away",
+    text: "A little known fact is that I cover about 40% of Africa."
+  },
+  {
+    pics: [
+      "https://images.unsplash.com/photo-1527628217451-b2414a1ee733?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      "https://images.unsplash.com/photo-1546728150-b3cbeddb6f6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    ],
+    name: "Heather",
+    age: 32,
+    distance: "4 miles away",
+    text:
+      "Respiratory Therapy Student. Cute enough to take your breath away, smart enough to bring it back"
+  }
+];
