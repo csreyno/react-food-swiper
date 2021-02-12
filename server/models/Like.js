ce1e4e335a9904d5e4b62fb56ac6afc7df33a444
starskreen619 <72418975+starskreen619@users.noreply.github.com>
2021-02-12 11:23:45 -0500

    bugfix for recipecontroller

--- server/models/Like.js
+++ server/models/Like.js
@@ -2,11 +2,6 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Like extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Like.belongsTo(models.User, {
         foreignKey: "user_id",
@@ -17,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
       models.User.belongsToMany(models.Recipe, {
         through: Like,
         foreignKey: "user_id",
-      }); //^^ Is this needed?
+      });
       models.Recipe.belongsToMany(models.User, {
         through: Like,
         foreignKey: "recipe_id",
