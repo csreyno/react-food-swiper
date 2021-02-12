4380200e5909e5218f6f4a5de1e9c9098ccf098c
Ryan Mount <rmount96@outlook.com>
2021-02-12 15:25:33 -0600

    Rendering favorite recipe data

--- client/src/components/pages/Favorites.jsx
+++ client/src/components/pages/Favorites.jsx
@@ -1,27 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import ResCard from "./ResCard";
 
 export default function Favorites(props) {
-  console.log(props)
+  const [show, setShow] = useState(false)
+  console.log(props.recipes)
   return (
     <div>
       <br />
       <br />
 
-      <div className="recipes">
+      <div className="myrecipes">
         <br />
         <h1 className="PageTitle">My Favorites</h1>
         <br />
                 <ul>
-          {props.recipes.map((recipes) => (
+          {props.recipes.map((r) => (
             <li
-              onClick={(e) => {
-                props.chooseRecipe(recipes.id);
-              }}
+              onClick={(e) => setShow(!show)} variant="contained"              
             >
-              {recipes.title}
+              {r.Recipe.title}
             </li>
+            
           ))}
+          {show ? (
+            <div>
+              {props.recipes.map((r) => (
+                <div>
+                <h3>{r.Recipe.title}</h3>
+                <div className="fav-image-div">
+                  <img className="fav-image" src={r.Recipe.image}></img>
+                </div>
+                <label>Ingredients</label>
+                <li>
+                  {r.Recipe.ingredients}
+                </li>
+                <li>
+                  {r.Recipe.preparation}
+                </li>
+
+                </div>
+          ))}
+            </div>
+          ) : null}
         </ul>
         <br />
         <ResCard />
