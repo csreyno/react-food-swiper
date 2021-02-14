20f1404ccb0a21d0e74296c1515904f527149218
starskreen619 <72418975+starskreen619@users.noreply.github.com>
2021-02-13 23:47:50 -0500

    style of favorites and my recipes' form

--- client/src/components/swiper/Deck.js
+++ client/src/components/swiper/Deck.js
@@ -2,9 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useSprings } from "react-spring/hooks";
 import { useGesture } from "react-with-gesture";
 import axios from 'axios'
-
 import Card from "./Card";
-
 import "../App.css";
 
 const to = (i) => ({
@@ -17,14 +15,13 @@ const to = (i) => ({
 const from = (i) => ({ rot: 0, scale: 1, y: -1000 });
 
 const trans = (r, s) =>
-  `perspective(1500px) rotateX(12deg) rotateY(${
-    r / 10
+  `perspective(1500px) rotateX(12deg) rotateY(${r / 10
   }deg) rotateZ(${r}deg) scale(${s})`;
 
 function Deck() {
   const [stack, setStack] = useState(0)
   const perStack = 10;
-  const [gone] = useState(() => new Set());  
+  const [gone] = useState(() => new Set());
   const [newRecipes, setNewRecipes] = useState([{}])
   const [currentRecipes, setCurrentRecipes] = useState([]);
   const [timestamp, setTimestamp] = useState(0);
@@ -33,7 +30,7 @@ function Deck() {
     ...to(i),
     from: from(i),
   }));
-  
+
   const importRecipes = async () => {
     const resp = await axios.get('/api/recipe-card')
     console.log(resp.data)
@@ -42,29 +39,29 @@ function Deck() {
   };
 
   const newLike = async (recipeid) => {
-    const resp = await axios.post('/api/members-only/addlike', { 
-    recipe_id: recipeid
+    const resp = await axios.post('/api/members-only/addlike', {
+      recipe_id: recipeid
     });
     console.log(resp.data)
     console.log(recipeid)
   }
-    
+
   const createStack = () => {
-  const cards = newRecipes.slice(stack*perStack, perStack*(stack+1)).map(r => {
-    let cardFormat = {
-      id: r.id,
-      title: r.title,
-      readyInMinutes: r.readyInMinutes,
-      image: r.image
-    }
-    return cardFormat
-  });
-  setCurrentRecipes(cards)
-  console.log("Current recipes updated", cards.length)
+    const cards = newRecipes.slice(stack * perStack, perStack * (stack + 1)).map(r => {
+      let cardFormat = {
+        id: r.id,
+        title: r.title,
+        readyInMinutes: r.readyInMinutes,
+        image: r.image
+      }
+      return cardFormat
+    });
+    setCurrentRecipes(cards)
+    console.log("Current recipes updated", cards.length)
   };
-  
+
   const nextStack = (s) => {
-    setStack(s+1);
+    setStack(s + 1);
   };
 
   // const likeCountInc = (like) => {
@@ -81,7 +78,7 @@ function Deck() {
 
   useEffect(() => {
     createStack();
-    
+
   }, [newRecipes, stack]);
 
   // useEffect(() => {
@@ -103,12 +100,12 @@ function Deck() {
     }
   }, [timestamp]);
 
-  
+
 
 
   const bind = useGesture(
     ({
-      args: [index, id], 
+      args: [index, id],
       down,
       delta: [xDelta],
       distance,
@@ -116,11 +113,11 @@ function Deck() {
       velocity,
     }) => {
       const trigger = velocity > 0.2;
-      
+
       const dir = xDir < 0 ? -1 : 1;
-      
+
       if (!down && trigger) gone.add(index);
-      
+
       set((i) => {
         console.log(id, "=======================")
         if (index !== i) return;
@@ -137,15 +134,15 @@ function Deck() {
           console.log(i);
           console.log("swiped left");
         }
-        
+
         const isGone = gone.has(index);
-        
+
         const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0;
-        
+
         const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0);
-        
+
         const scale = down ? 1.1 : 1;
-        
+
         return {
           x,
           rot,
@@ -154,29 +151,30 @@ function Deck() {
           config: { friction: 20, tension: down ? 800 : isGone ? 200 : 500 },
         };
       });
-      
+
       if (!down && gone.size === perStack) {  //=== data.length
         setTimeout(() => gone.clear() || set((i) => to(i)), 600);
         setTimestamp((new Date()).getTime());
-        
+
       }
     }
   );
 
-  return props.map(({ x, y, rot, scale }, i) =>{
-   return (
-    <Card
-      key={i}
-      i={i}
-      x={x}
-      y={y}
-      rot={rot}
-      scale={scale}
-      trans={trans}
-      data={currentRecipes}
-      bind={bind}
-    />
-  )});
+  return props.map(({ x, y, rot, scale }, i) => {
+    return (
+      <Card
+        key={i}
+        i={i}
+        x={x}
+        y={y}
+        rot={rot}
+        scale={scale}
+        trans={trans}
+        data={currentRecipes}
+        bind={bind}
+      />
+    )
+  });
 }
 
 export default Deck;
