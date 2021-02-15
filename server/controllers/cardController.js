cd3034c327e0cd0d2bc0d5cf856c553679d12389
Ryan Mount <rmount96@outlook.com>
2021-02-15 15:48:27 -0600

    card rendering

--- server/controllers/cardController.js
+++ server/controllers/cardController.js
@@ -4,7 +4,7 @@ const generateCard = async (req, res) => {
   // const { id } = req.session.user;
   // const { recipeid } = req.body
 
-  const cards = await Recipe.findAll({limit: 50})
+  const cards = await Recipe.findAll()//{limit: 50}
   return res.status(200).json(cards);
 
 }
