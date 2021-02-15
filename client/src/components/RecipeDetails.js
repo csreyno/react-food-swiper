65bc7ec727380a5ae670eb32df70ce4e13722174
Ryan Mount <rmount96@outlook.com>
2021-02-14 23:23:59 -0600

    recipe list styling changes

--- client/src/components/RecipeDetails.js
+++ client/src/components/RecipeDetails.js
@@ -3,35 +3,9 @@ import Button from "@material-ui/core/Button";
 const RecipeList = (props) => {
   const prep = props.preparation.split(".");
   const ingredients = props.ingredients
-  console.log(prep);
 
   return (
-//         <div className="fav-div">
-//           <div className="title-div">
-//             <h3 className="fav-title" onClick={props.hide}>{props.title}</h3>
-
-//           </div>
-//           <div className="img-div">
-            
-//             <img className="fav-image" src={props.image}></img>
-//             </div>
-//           <div className="ing-div">
-//             <h4 className="fav-label">Ingredients</h4>
-//               <ul>
-//                 {ingredients.map(i => <li className="ingredient-list">{i}</li>)}
-//               </ul>
-//           </div>
-//           <div className="prep-div">
-//             <h4 className="fav-label">Preparation</h4>
-//               <ol >
-//                 {prep.map(p => <li className="prep-list">{p}</li>)}
-//               </ol>
-//           </div>
-//         </div>
-//     );
-// };
-        <div className="fav-div">
-
+  <div className="fav-div">
     <div className="favcontainer" id="favcontainer">
       <div className="favcontinner" id="favcontinner">
         <div onClick={props.hide} className="Favline">
