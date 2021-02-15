05cfc0970fcf85e9e2fd1dab5e35ea33e2ad6fc9
Ryan Mount <rmount96@outlook.com>
2021-02-15 16:14:46 -0600

    final styling changes

--- server/controllers/cardController.js
+++ server/controllers/cardController.js
@@ -1,9 +1,6 @@
 const { Recipe } = require('../models')
 
 const generateCard = async (req, res) => {
-  // const { id } = req.session.user;
-  // const { recipeid } = req.body
-
   const cards = await Recipe.findAll()//{limit: 50}
   return res.status(200).json(cards);
 
