d5538244d18366c8e64e066e2a5dd0e349491c38
Chad Reynolds <chad.s.reynolds@gmail.com>
2021-02-15 16:28:56 -0500

    choose file button centered

--- client/src/components/pages/RecipeForm2.jsx
+++ client/src/components/pages/RecipeForm2.jsx
@@ -107,7 +107,7 @@ export default function RecipeForm2(props) {
         </Button>
         </div>
         {show ? (
-          <div>
+          <div className="form1">
             <TextField
               id="filled-basic"
               value={title}
@@ -177,6 +177,7 @@ export default function RecipeForm2(props) {
               onChange={(e) => {
                 setImage(e.target.files[0]);
               }}/>
+              <label htmlFor="icon-button-file">Choose File</label>
             </div>
             
             <label htmlFor="icon-button-file">
